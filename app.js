037b45843da64d3b5eee3b4ab4e1f429a70fc82a
AirFluke <111533550+AirFluke@users.noreply.github.com>
2023-07-21 08:21:25 -0700

    The First Commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs")
+const bodyparser= require("body-parser")
+const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption")
+const app = express();
+
+
+app.use(express.static("public"))
+app.use(bodyparser.urlencoded({extended:true}))
+app.set("view engine", "ejs")
+mongoose.connect("mongodb://127.0.0.1:27017/usersDB", {useNewUrlParser : true})
+
+const userSchema =  new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields:["password"]})
+
+const User = new mongoose.model("User", userSchema)
+
+
+
+app.get("/", (req,res)=>{
+    res.render("home")
+})
+
+app.get("/login", (req,res)=>{
+    res.render("login")
+})
+
+app.get("/register", (req,res)=>{
+    res.render("register")
+})
+
+app.post("/register", (req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+
+    })
+    newUser.save(res.render("secrets"));
+})
+
+    
+
+
+
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+
+    try {
+        const foundUser = await User.findOne({email : username}) 
+         if(foundUser.password === password){
+            res.render("secrets")
+        }
+    
+    } catch (err) {console.log(err) }
+
+});
+
+
+    
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("server started")
+})
